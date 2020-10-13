47adfa5625a514d276f6c58f4731ecf67c561db1
catdrinkcola <1160977981@qq.com>
2020-10-13 13:43:11 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Core.h"
+namespace Hazel
+{
+	class HAZEL_API Application
+	{
+	public:
+		Application();
+		~Application();
+
+		void Run();
+	};
+
+	//To be define in CLIENT
+	Application* CreateApplication();
+}
